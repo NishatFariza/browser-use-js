9f91598b3d9ccf8973769e266996acc859a5af2f
NishatFariza <nishatfarzia78@gmail.com>
2022-03-05 12:32:36 +0600

    arrow function in setTime out

---
+++ timeout.js
@@ -0,0 +1,17 @@
+const doSomething = () => console.log('I am coding javaScript');
+
+
+console.log('First: This is first one');
+console.log('Second: This is second one');
+console.log('Third: This is third one');
+ setTimeout (function (){
+     console.log('This the set time out function');
+     
+ }, 5000)
+ setTimeout ( () => {
+     console.log('This the set time out function And I am using vs code');
+     
+ }, 3000)
+
+// doSomething()
+console.log('Fourth: This is fourth numbers');
